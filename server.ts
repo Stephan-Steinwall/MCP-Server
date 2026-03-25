7c5c9ffcad2847486b2cb47e8bcc3f3c320e0c0b
Stephan-Steinwall <staienwallthilina@gmail.com>
2026-03-26 02:19:29 +0530

    feat: Add `getCurrentWeather` tool to the server.

--- server.ts
+++ server.ts
@@ -5,3 +5,21 @@ const server = new McpServer({
     name: 'weather data fetcher',
     version: '1.0.0'
 })
+
+server.tool(
+    'getCurrentWeather',
+    'Get the weather of a given city',
+    {
+        city: z.string().describe('The city to get the weather for')
+    },
+    async ({ city }) => {
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: `The weather in ${city} is sunny`
+                }
+            ]
+        }
+    }
+)
